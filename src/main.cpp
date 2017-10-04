314ad336b03334d7ca8cebbce86f3bfa871bab36
Radhika Mattoo <rm3485@nyu.edu>
2017-10-04 14:23:24 -0400

    separated sections into functions, working on 1.3

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+// Radhika Mattoo, rm3485@nyu.edu
+// Computer Graphics Fall 2017
+// Assignment 1
+
 // C++ include
 #include <iostream>
 #include <string>
@@ -24,7 +28,7 @@ double get_discriminant(Vector3d &ray_origin, Vector3d &ray_direction, Vector3d 
 
   return discriminant;
 }
-
+// Gets a ray's intersection point with sphere
 Vector3d get_intersection(double discriminant, Vector3d &ray_direction, Vector3d &ray_origin, Vector3d &sphere_center)
 {
   double A_ = ray_direction.dot(ray_direction);
@@ -44,7 +48,7 @@ Vector3d get_intersection(double discriminant, Vector3d &ray_direction, Vector3d
   }
   return ray_intersection;
 }
-
+// Determines the color for a given pixel using its ray's intersection point
 double get_pixel_color(bool diffuse, double discriminant, double sphere_radius, Vector3d &origin, Vector3d light_position, Vector3d &ray_direction, Vector3d &ray_origin, Vector3d &sphere_center)
 {
   double pixel_value;
@@ -92,33 +96,63 @@ double get_pixel_color(bool diffuse, double discriminant, double sphere_radius, 
   return pixel_value;
 }
 
-void part1()
+//
+void perspective_projection()
 {
-    // std::cout << "Part 1: Writing a grid png image" << std::endl;
 
-    const std::string filename("part1.png");
-    Eigen::MatrixXd M(800,800);
-
-    // Draw a grid, each square has a side of e pixels
-    const int e = 50;
-    const double black = 0;
-    const double white = 1;
-
-    for (unsigned wi = 0; wi<M.cols();++wi)
-        for (unsigned hi = 0; hi < M.rows(); ++hi)
-            M(hi,wi) = (lround(wi / e) % 2) == (lround(hi / e) % 2) ? black : white;
-
-    // Write it in a png image. Note that the alpha channel is reversed to make the white (color = 1) pixels transparent (alhpa = 0)
-    write_matrix_to_png(M,M,M,1.0-M.array(),filename);
 }
 
+// 1.1 Ray Tracing Spheres
+void part1()
+{
+  std::cout << "Part 1.1: Ray Tracing Spheres" << std::endl;
+  MatrixXd C = MatrixXd::Zero(800,800);
+  const std::string filename("part1.png");
 
+  MatrixXd A = MatrixXd::Zero(800,800); // Store the alpha mask
+
+  // The camera is orthographic, pointing in the direction -z and covering the unit square (-1,1) in x and y
+  Vector3d origin(-1,1,1);
+  Vector3d x_displacement(2.0/A.cols(),0,0);
+  Vector3d y_displacement(0,-2.0/A.rows(),0);
+
+  // CHANGE THIS LINE FOR NON-CENTERED SPHERE
+  Vector3d sphere_center = RowVector3d(0,0,0);
+  const double sphere_radius = 0.9;
+
+  const Vector3d light_position(-1,1,1);
+  const bool diffuse = true;
+
+  for (unsigned i=0;i<C.cols();i++)
+  {
+      for (unsigned j=0;j<C.rows();j++)
+      {
+          // Prepare the ray
+          Vector3d ray_origin = origin + double(i)*x_displacement + double(j)*y_displacement;
+          Vector3d ray_direction = RowVector3d(0,0,-1);
+
+          // Find discriminant and intersection point
+          double discriminant =  get_discriminant(ray_origin, ray_direction, sphere_center, sphere_radius);
+          if(discriminant >= 0){
+            C(i,j) = get_pixel_color(diffuse, discriminant, sphere_radius, origin, light_position, ray_direction, ray_origin, sphere_center);
+          }else{
+            C(i,j) = 1.0;
+          }
+          // Disable the alpha mask for this pixel
+          A(i,j) = 1;
+      } // inner loop
+  } // outer loop
+  std::cout << "Saving 1.1 sphere to png" << std::endl;
+  write_matrix_to_png(C,C,C,A,filename);
+}
+
+// 1.2 Shading
 void part2()
 {
-  std::cout << "Part 2: Simple ray tracer, one sphere with orthographic projection" << std::endl;
+  std::cout << "Part 1.2: Shading" << std::endl;
 
     const std::string filename("part2.png");
-    // MatrixXd C = MatrixXd::Zero(800,800);
+
     // Store the color
     MatrixXd R = MatrixXd::Zero(800,800);
     MatrixXd G = MatrixXd::Zero(800,800);
@@ -131,8 +165,7 @@ void part2()
     Vector3d x_displacement(2.0/A.cols(),0,0);
     Vector3d y_displacement(0,-2.0/A.rows(),0);
 
-    // TODO: Render multiple spheres with different colors
-    // One should have specular  and one should have diffuse lighting
+    // One sphere should have specular, and one only diffuse lighting
     Vector3d sphere_center = RowVector3d(-0.5,0,0);
     Vector3d sphere_center_2 = RowVector3d(0.5,0,0);
     const double sphere_radius = 0.4;
@@ -174,15 +207,79 @@ void part2()
 
         } // inner loop
     } // outer loop
-    std::cout << "Saving to png";
+    std::cout << "Saving sphere 1.2 to png" << std::endl;
+    write_matrix_to_png(R,G,B,A,filename);
+}
+// 1.3 Perspective Projection
+void part3()
+{
+  std::cout << "Part 1.3: Perspective Projection" << std::endl;
+
+    const std::string filename("part3.png");
+
+    // Store the color
+    MatrixXd R = MatrixXd::Zero(800,800);
+    MatrixXd G = MatrixXd::Zero(800,800);
+    MatrixXd B = MatrixXd::Zero(800,800);
+
+    MatrixXd A = MatrixXd::Zero(800,800); // Store the alpha mask
+
+    // The camera is orthographic, pointing in the direction -z and covering the unit square (-1,1) in x and y
+    Vector3d origin(-1,1,1);
+    Vector3d x_displacement(2.0/A.cols(),0,0);
+    Vector3d y_displacement(0,-2.0/A.rows(),0);
+
+    // One sphere should have specular, and one only diffuse lighting
+    Vector3d sphere_center = RowVector3d(-0.5,0,0);
+    Vector3d sphere_center_2 = RowVector3d(0.5,0,0);
+    const double sphere_radius = 0.4;
+
+    const Vector3d light_position(-1,2,1);
+    const Vector3d light_position_2(1, -2, -1);
+
+    bool diffuse;
+    for (unsigned i=0;i<A.cols();i++)
+    {
+        for (unsigned j=0;j<A.rows();j++)
+        {
+            // Prepare the ray
+            Vector3d ray_origin = origin;
+            Vector3d ray_direction =  (origin + double(i)*x_displacement + double(j)*y_displacement) - origin;
+
+            // Find discriminants to determine if there's a solution
+            double discriminant =  get_discriminant(ray_origin, ray_direction, sphere_center, sphere_radius);
+            double discriminant_2 = get_discriminant(ray_origin, ray_direction, sphere_center_2, sphere_radius);
+
+            if(discriminant >= 0){
+              diffuse = true; // one sphere should have only diffuse
+              R(i,j) = get_pixel_color(diffuse, discriminant, sphere_radius, origin, light_position, ray_direction, ray_origin, sphere_center);
+              R(i,j) += get_pixel_color(diffuse, discriminant, sphere_radius, origin, light_position_2, ray_direction, ray_origin, sphere_center);
+              R(i,j) -= 0.1; //only supposed to count ambient lighting once
+            }else if(discriminant_2 >= 0){
+              diffuse = false; // the other sphere should be specular
+              G(i,j) = get_pixel_color(diffuse, discriminant_2, sphere_radius, origin, light_position, ray_direction, ray_origin, sphere_center_2);
+              G(i,j) += get_pixel_color(diffuse, discriminant_2, sphere_radius, origin, light_position_2, ray_direction, ray_origin, sphere_center_2);
+              G(i,j) -= 0.1; //only supposed to count ambient lighting once
+            }else{
+              R(i,j) = 1.0;
+              G(i,j) = 1.0;
+              B(i,j) = 1.0;
+            }
+
+            // Disable the alpha mask for this pixel
+            A(i,j) = 1;
+
+        } // inner loop
+    } // outer loop
+    std::cout << "Saving sphere 1.3 to png" << std::endl;
     write_matrix_to_png(R,G,B,A,filename);
 }
 
-
 int main()
 {
-    part1();
-    part2();
+    // part1();
+    // part2();
+    part3();
 
     return 0;
 }
